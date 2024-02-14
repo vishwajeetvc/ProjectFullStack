4467be6b7ddafe5e14b6a24195bd4a073c54c1a3
vishwajeet Kumar <vishwajeetk@localhost.localdomain>
2024-02-14 12:29:16 +0530

    DataBAse connection(local)

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 
 import express from 'express'
-
 const app = express();
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017')
+.then(()=>{
+    console.log("Connected to dataBase successfully")
+}).catch(console.log)
 
 
 
